a96ef924e8972377b0fa64ccfd36fa6c9e4e0f20
brandon dalesandro <brandonadalesandro@gmail.com>
2014-11-03 00:09:33 -0800

    added post handling

--- server.js
+++ server.js
@@ -3,10 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) {
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log("request for " + pathname + " recieved.");
         
-        route(handle, pathname, response);   
+        request.setEncoding("utf8");
+         
+        request.addListener("data", function(postDataChunk) {
+            postData += postDataChunk;
+            console.log("recieved POST data chunk '" + postDataChunk + "' .");
+        });
+        
+        request.addListener("end", function() {
+            route(handle, pathname, response, postData);
+        });
     }
 
     http.createServer(onRequest).listen(7777);
